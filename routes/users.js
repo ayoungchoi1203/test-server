5e90bdd9e839ad0603a85b800604188b87c82ecd
ayoungchoi <aychoi1203@gmail.com>
2020-07-17 22:10:07 +0900

    fix bug

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('이건 세번째 branch야');
+  res.send('이건 세번째 branch야;;;;;');
 });
 
 router.get('/bbb', function(req, res, next) {
